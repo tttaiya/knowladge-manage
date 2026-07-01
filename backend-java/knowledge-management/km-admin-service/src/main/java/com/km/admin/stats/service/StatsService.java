71f7ed06c52b1d82a3953c642f783ff22a499f21
jeffreyyang39 <jeffreyyang39@gmail.com>
2026-07-01 15:43:03 +0800

    f8统计信息的前端与后端都在这个分支里了。 希望最后不要出问题了。

--- backend-java/knowledge-management/km-admin-service/src/main/java/com/km/admin/stats/service/StatsService.java
+++
@@ -1,8 +0,0 @@
-// service/StatsService.java
-package com.km.admin.stats.service;
-
-import com.km.admin.stats.dto.StatsOverviewDTO;
-
-public interface StatsService {
-    StatsOverviewDTO getStatsOverview(Integer days);
-}
